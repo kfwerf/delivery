874ea6ee35df939e6c026587782e8b3697f96219
Kenneth van der Werf <kennethv@spotify.com>
2021-03-14 11:13:43 +0100

    Add body and result editors

---
+++ src/app/components/Body.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import AceEditor from "react-ace";
+import {useDispatch, useSelector} from "react-redux";
+import GrpcTypeRegistry from "../../registry/registry";
+
+import "ace-builds/src-noconflict/mode-json";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/ext-beautify";
+import "ace-builds/src-noconflict/ext-language_tools";
+import {updateBody} from "../actions/request";
+
+export default function Body() {
+    const generatedName = `body-${Math.random().toString(36).substr(2, 9)}`;
+    const generatedNameInput = `${generatedName}-textarea`;
+
+    const method: string = useSelector((state) => {
+        // @ts-ignore
+        return state?.request?.method;
+    });
+
+    const typeRegistry: GrpcTypeRegistry = useSelector((state) => {
+        // @ts-ignore
+        return state?.introspection?.typeRegistry;
+    });
+
+    const isDisabled: boolean = useSelector((state) => {
+        // @ts-ignore
+        return state?.request?.url?.length < 1 || state?.request?.method?.length < 1 || !state?.introspection?.isLoading;
+    });
+
+    const rpc = typeRegistry?.getRpc(method);
+    const requestMethod = rpc?.getRequest();
+    const message = typeRegistry?.getMessage(requestMethod);
+    const value = message?.getExample(typeRegistry) || {};
+
+
+    const dispatch = useDispatch();
+    const onChange = (body: string) => {
+        try {
+            dispatch(updateBody(JSON.parse(JSON.stringify(body))));
+        } catch(error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <div className={generatedName} id={generatedNameInput}>
+            <div className="form-group">
+                <label htmlFor={generatedNameInput}>Body</label>
+                <AceEditor
+                    mode="json"
+                    theme="github"
+                    placeholder="Body"
+                    onChange={onChange}
+                    value={JSON.stringify(value, null, 4)}
+                    name={generatedNameInput}
+                    editorProps={{ $blockScrolling: true }}
+                    setOptions={{
+                        enableBasicAutocompletion: true,
+                        enableLiveAutocompletion: true,
+                    }}
+                    height="446px"
+                    width="100%"
+                    readOnly={isDisabled}
+                />
+            </div>
+        </div>
+    )
+}
---
+++ src/app/components/Result.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import AceEditor from "react-ace";
+import {useDispatch, useSelector} from "react-redux";
+import GrpcTypeRegistry from "../../registry/registry";
+
+import "ace-builds/src-noconflict/mode-json";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/ext-beautify";
+import "ace-builds/src-noconflict/ext-language_tools";
+import {updateBody} from "../actions/request";
+import GrpCurlResponse from "../../models/GrpCurlResponse";
+
+export default function Result() {
+    const generatedName = `result-${Math.random().toString(36).substr(2, 9)}`;
+    const generatedNameInput = `${generatedName}-textarea`;
+
+    const response: GrpCurlResponse = useSelector((state) => {
+        // @ts-ignore
+        const response: GrpCurlResponse = state?.request?.response;
+        return response;
+    });
+
+    let value = '';
+    if (response.hasError()) {
+        value = response.getError();
+    } else {
+        try {
+            value = JSON.stringify(JSON.parse(response.getData()), null, 4);
+        } catch(ignore) {
+            value = response.getData();
+        }
+    }
+
+    console.log(response);
+
+    return (
+        <div className={generatedName} id={generatedNameInput}>
+            <AceEditor
+                mode="json"
+                theme="github"
+                placeholder="Response"
+                wrapEnabled={true}
+                readOnly={true}
+                value={value}
+                width="100%"
+                height="650px"
+                highlightActiveLine={false}
+                name={generatedNameInput}
+                editorProps={{ $blockScrolling: true }}
+                setOptions={{
+                    enableBasicAutocompletion: false,
+                    enableLiveAutocompletion: false,
+                }}
+            />
+        </div>
+    )
+}
