3d0e2c7b21dbe9017ec4749653d3569d9ce1c053
Kenneth van der Werf <kennethv@spotify.com>
2021-03-14 11:10:25 +0100

    Add base config and remove older files

---
+++ src/bootstrap.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createEpicMiddleware } from 'redux-observable';
+import { Provider } from 'react-redux';
+
+import App from './app/App';
+import { rootReducer } from './app/reducers';
+import { rootEpic } from './app/epics';
+
+const composeEnhancer = compose;//window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const epicMiddleware = createEpicMiddleware();
+
+
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(epicMiddleware)));
+
+epicMiddleware.run(rootEpic);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
